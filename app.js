65da1af5acbe2ceb719e2975cbb89ba98536f1e3
Olivia Lewis <ollybaby12@gmail.com>
2021-03-05 17:48:13 -0500

    lotto drill

--- app.js
+++ app.js
@@ -132,6 +132,68 @@ app.get('/cipher', (req, res) => {
 });
 
 app.get('/lotto', (req, res) => {
-    const numbers = 'numbers';
+    const { numbers } = req.query;
 
-})
+    //validation:
+    //1. the numbers array must exist
+    //2. must be an array
+    //3. must be 6 numbers
+    //4. numbers must be between 1 and 20
+
+    if(!numbers) {
+        return res.status(400).send("numbers is required");
+    }
+    if(!Array.isArray(numbers)) {
+        return res.status(400).send("numbers must be an array");
+    }
+
+    const guesses = numbers.map(n => parseInt(n))
+        .filter(n => !Number.isNaN(n) && (n >= 1 && n <=20));
+
+    if(guesses.length != 6) {
+        return res.status(400).send("numbers must contain 6 integers between 1 and 20");
+    }
+
+    //fully validated numbers
+    //here are the 20 numbers to chose from
+    const stockNumbers = Array(20).fill(1).map((_, i) => i + 1);
+
+    //randomly chose 6
+    const winningNumbers = [];
+    for(let i =0; i < 6; i++) {
+        const ran = Math.floor(Math.random() * stockNumbers.length);
+        winningNumbers.push(stockNumbers[ran]);
+        stockNumbers.splice(ran, 1);
+    }
+
+    //compare the guesses to the winning number
+    let diff = winningNumbers.filter(n => !guesses.includes(n));
+
+    //construct a response
+
+    let responseText;
+
+    switch(diff.length){
+        case 0:
+            responseText = 'Wow! Unbelievable! You could have won the mega millions!';
+            break;
+        case 1:
+            responseText = 'Congratulations! You win $100!';
+            break;
+        case 2:
+            responseText = 'Congratulations, you win a free ticket!';
+            break;
+        default:
+            responseText = 'Sorry, you lose';
+    }
+
+    //uncomment below to see how the results ran
+    // res.json({
+    //     guesses,
+    //     winningNumbers,
+    //     diff,
+    //     responseText
+    // });
+
+    res.send(responseText);
+});
