2780c5119faa6fd7de19dde8fbfd7fe45121072a
Olivia Lewis <ollybaby12@gmail.com>
2021-03-08 02:22:58 -0500

    grade path

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // This is middleware that requests pass through
 // on their way to the final handler
-app.use(morgan('dev'));
+app.use(morgan('dev')); 
 
 //This is the final request handler
 app.get('/', (req, res) => {
@@ -196,4 +196,78 @@ app.get('/lotto', (req, res) => {
     // });
 
     res.send(responseText);
+});
+
+app.get('/hello', (req, res) => {
+    res
+        .status(204)
+        .end();
+});
+
+app.get('/video', (req, res) => {
+    const video = {
+        title: 'Cats falling over',
+        description: '15 minutes of hilarious fun as cats fall over',
+        length: '15.40'
+    }
+    res.json(video);
+});
+
+app.get('/colors', (req, res) => {
+    const colors = [
+        {
+            name: "red",
+            rgb: "FF0000"
+        },
+        {
+            name: "green",
+            rgb: "00FF00",
+        },
+        {
+            name: "blue",
+            rgb: "0000FF"
+        },
+    ];
+    res.json(colors);
+});
+
+app.get('/grade', (req, res) => {
+    //get the mark from the query 
+    const { mark } = req.query;
+
+    //do some validation
+    if(!mark) {
+        //mark is required
+        return res
+            .status(400)
+            .send('Please provide a mark');
+    }
+
+    const numericMark = parseFloat(mark);
+    if (Number.isNaN(numericMark)) {
+        //mark must be a number
+        return res
+            .status(400)
+            .send('Mark must be a numeric value');
+    }
+
+    if (numericMark < 0 || numericMark > 100) {
+        return res
+            .status(400)
+            .send('Mark must be in the range 0 to 100');
+    }
+
+    if (numericMark >= 90) {
+        return res.send('A');
+    }
+
+    if (numericMark >= 80) {
+        return res.send('B');
+    }
+
+    if (numericMark >= 70) {
+        return res.send('C');
+    }
+
+    res.send('F');
 });
